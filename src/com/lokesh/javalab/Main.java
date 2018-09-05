79198e46f8495a82a061b9fa48f6836ee9505bb9
Lokesh <bandhulokesh@gmail.com>
2018-09-05 14:55:13 -0500

    Initial commit

---
+++ src/com/lokesh/javalab/Main.java
@@ -0,0 +1,17 @@
+package com.lokesh.javalab;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println("POST INCREMENT" + a++);
+
+        int b = 10;
+
+        System.out.println("PRE INCREMENT" + ++b);
+
+
+    }
+}
